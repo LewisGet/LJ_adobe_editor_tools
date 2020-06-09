757befc1073a142b7e98454e0b299936480a0b07
Lewis Jang <lewis.aedes@gmail.com>
2020-06-09 14:18:27 +0800

    add some note

---
+++ readme.md
@@ -0,0 +1,3 @@
+## This is adobe premiere pro script for LJMedia.
+
+auto injection Tensorflow output in adobe premiere pro.
--- test_codes.jsx
+++ test_codes.jsx
@@ -41,8 +41,27 @@ for (var i = 0; i < track_2_clip_1_item_1.videoComponents.numItems; i++)
     video_components[i.toString()] = track_2_clip_1_item_1.videoComponents[i];
 }
 
+/**
+    projectItem.createSubClip
+    1. clone item name.
+    2. start time ( in clip )
+    3. end time ( in clip )
+    4. time extends to the end of the video
+    5. session counter???  in don't know that it is.
+    6. clone video
+    7. clone audio
+ */
 var item2 = track_2_clip_1_item_1.createSubClip("hello", 0, 5, 1, 1, 1, 1);
 
 track_2.insertClip(item2, 5);
 track_2.insertClip(item2, 6);
 track_2.insertClip(item2, 7);
+
+/**
+    app.project.importFiles
+    1. array file paths
+    2. suppress warnings
+    3. input to where
+    4. import as numbered stills
+ */
+app.project.importFiles(["D:\\code\\LJ_adobe_editor_tools\\test_video2.mp4"], 1, app.project.rootItem, 0);
