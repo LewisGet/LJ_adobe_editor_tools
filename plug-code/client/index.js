357137472392314209b5e7aa2232c3aacf10040b
Lewis Jang <lewis.aedes@gmail.com>
2020-06-17 16:58:44 +0800

    add backend api and ui function

--- plug-code/client/index.js
+++ plug-code/client/index.js
@@ -1,11 +1,19 @@
-/* 1) Create an instance of CSInterface. */
 var cs_interface = new CSInterface();
 
-/* 2) Make a reference to your HTML button and add a click handler. */
-var open_button = document.querySelector("#open-button");
-open_button.addEventListener("click", open_doc);
+var clone_lewis = document.querySelector("#clone-lewis");
 
-/* 3) Write a helper function to pass instructions to the ExtendScript side. */
-function open_doc() {
-  cs_interface.evalScript("open_document()");
-}
+clone_lewis.addEventListener("click", function(){
+    cs_interface.evalScript("clone_lewis()");
+});
+
+var clone_kevin = document.querySelector("#clone-kevin");
+
+clone_kevin.addEventListener("click", function(){
+    cs_interface.evalScript("clone_kevin()");
+});
+
+var clone_gold = document.querySelector("#clone-gold");
+
+clone_gold.addEventListener("click", function(){
+    cs_interface.evalScript("clone_gold()");
+});
--- plug-code/host/main.jsx
+++ plug-code/host/main.jsx
@@ -1,4 +1,28 @@
-function open_document(){
-  var fileRef = new File("~/Downloads/myFile.jpg");
-  var docRef = app.open(fileRef);
+var active_sequence = app.project.activeSequence;
+var tracks = app.project.activeSequence.videoTracks;
+var total_tracks = tracks.numTracks;
+var total_tracks_for_array = tracks.numTracks - 1;
+
+function get_clip(track_sort, clip_sort)
+{
+    return (tracks[track_sort]).clips[clip_sort];
+}
+
+
+function clone_lewis()
+{
+    // 從最後列數回來
+    var add_in = 0;
+    var insert_track_index = total_tracks_for_array - add_in;
+    var org_track_index = insert_track_index - 3;
+
+    var clip = get_clip(org_track_index, 0);
+    var insert_time = active_sequence.getPlayerPosition().seconds;
+
+    var clip_name = "lewis_" + (parseInt(insert_time)).toString();
+    var clone_item = clip.projectItem.createSubClip(clip_name, 0, 5, 1, 1, 1, 1);
+
+    var insert_track = tracks[insert_track_index];
+
+    insert_track.insertClip(clone_item, insert_time);
 }
