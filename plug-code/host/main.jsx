361e44dd3af036da58f80734ec27f003b4fa6a12
Lewis Jang <lewis.aedes@gmail.com>
2020-06-17 17:05:03 +0800

    add backend api and ui function

--- plug-code/host/main.jsx
+++ plug-code/host/main.jsx
@@ -9,20 +9,35 @@ function get_clip(track_sort, clip_sort)
 }
 
 
-function clone_lewis()
+function clone_clip(name, start_at)
 {
-    // 從最後列數回來
-    var add_in = 0;
-    var insert_track_index = total_tracks_for_array - add_in;
+    // start_at 是從最後列數回來的 track index
+
+    var insert_track_index = total_tracks_for_array - start_at;
     var org_track_index = insert_track_index - 3;
 
     var clip = get_clip(org_track_index, 0);
     var insert_time = active_sequence.getPlayerPosition().seconds;
 
-    var clip_name = "lewis_" + (parseInt(insert_time)).toString();
+    var clip_name = name + "_" + (parseInt(insert_time)).toString();
     var clone_item = clip.projectItem.createSubClip(clip_name, 0, 5, 1, 1, 1, 1);
 
     var insert_track = tracks[insert_track_index];
 
     insert_track.insertClip(clone_item, insert_time);
+}
+
+function clone_lewis()
+{
+    clone_clip("lewis", 0);
+}
+
+function clone_kevin()
+{
+    clone_clip("kevin", 1);
+}
+
+function clone_gold()
+{
+    clone_clip("gold", 2);
 }
